be33ba40743ebc3b3cdea258fd19cd9862205e3a
Jonathan Xia <jono@fastmail.fm>
2024-05-20 15:58:07 -0700

    Load-shifting: deleted obsolete cache table models

--- django_framework/settings/base.py
+++ django_framework/settings/base.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'geopportunity',
-    #'load_shifting'
+    'load_shifting'
 ]
 
 MIDDLEWARE = [
--- load_shifting/models.py
+++ load_shifting/models.py
@@ -1,35 +1,6 @@
 from django.db import models
 
 
-class EIAHourlyDataCache(models.Model):
-    
-    cached_date = models.DateTimeField("date cached")
-
-    # Following 4 constitute the cache key
-    url_segment = models.CharField(max_length=200)
-    facets = models.TextField() # store json blob
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    # TODO add unique constraint on the 4 of them?
-
-    # the content, in CSV
-    response_csv = models.TextField()
-    
-    
-
-# separate cache for raw eia data and our derived data?
-
-class HourlyGenerationMixCache(models.Model):
-    # this is for post-import/export corrected data
-    cached_date = models.DateTimeField("date cached")
-    ba_name = models.CharField(max_length=32)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    calculated_csv = models.TextField()
-    # each row of CSV is that x MWh were generated from Y fuel type in hour Z on balancing authority W and emitted
-    # Q tons of CO2
-
-
 class AllPurposeCSVCache(models.Model):
     cache_function_name = models.TextField()
     cached_date = models.DateTimeField("date cached")
